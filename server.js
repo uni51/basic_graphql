443a19428490bdefef88c4270c18b8074901a458
Koichi.Shimomura <unieye51@gmail.com>
2022-03-23 08:53:48 +0900

    2-3 Basic Types

--- server.js
+++ server.js
@@ -5,18 +5,27 @@ const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 
 // GraphQL スキーマ言語を記述してスキーマを構築する
-// スキーマはあくまで定義のみで実際のデータ操作は行わない
 const schema = buildSchema(`
   type Query {
-    hello: String
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
   }
 `);
 
 // リゾルバ関数
-// リゾルバ関数とは特定のフィールドのデータを返す関数（メソッド）であり、実際のデータ操作を行う部分
 const root = {
-  hello: () => {
-    return 'Hello world!';
+  // スキーマで定義した「quoteOfTheDay: String」のデータ操作
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+  },
+  // スキーマで定義した「random: Float!」のデータ操作
+  random: () => {
+    return Math.random();
+  },
+  // スキーマで定義した「rollThreeDice: [Int]」のデータ操作
+  rollThreeDice: () => {
+    return [1, 2, 3].map((_) => 1 + Math.floor(Math.random() * 6));
   },
 };
 
